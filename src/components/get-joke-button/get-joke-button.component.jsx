7d496c790c722a273d960190d5ed83389efc30d4
Arvid Lindström <arvid-lindstrom@live.se>
2024-02-21 21:14:13 +0100

    initial work on get-joke-button component

---
+++ src/components/get-joke-button/get-joke-button.component.jsx
@@ -0,0 +1,14 @@
+
+const getJoke = () => {
+  
+}
+
+const GetJokeButton = props => {
+  return (
+    <div className="get-joke-button" onClick={getJoke}>
+      <h1>Get joke</h1>
+    </div>
+  )
+}
+
+export default GetJokeButton;
