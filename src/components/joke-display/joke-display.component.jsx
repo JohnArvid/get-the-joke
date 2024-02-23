9dded0873396b08a6a3debe6ea1b8bdafbd261e0
JohnArvid <arvid-lindstrom@live.se>
2024-02-23 10:11:38 +0100

    Make joke-display.component actually return something...

--- src/components/joke-display/joke-display.component.jsx
+++ src/components/joke-display/joke-display.component.jsx
@@ -1,9 +1,9 @@
 import './joke-display.styles.css'
 
-const JokeDisplay = ({joke}) => {
+const JokeDisplay = ({joke}) => (
     <div className='joke-display'>
         <p>{joke}</p>
     </div>
-}
+)
 
 export default JokeDisplay;
